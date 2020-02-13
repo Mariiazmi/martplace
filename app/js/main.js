d7699e1bbbccb79ec2cec95419dbe099bab85cee
OK <masha.zmi@gmail.com>
2020-02-13 16:18:51 +0200

    added new produts

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,13 @@ $(function(){
     $('.featured__slider').slick({
         
       });
+
+      $(".product__item-star").rateYo({
+        rating: 4.5,
+        starWidth: "13px",
+        spacing: "3px",
+        readOnly: true,
+        ratedFill: "#ffc000",
+      });
+      var mixer = mixitup('.new__slider');
 });
