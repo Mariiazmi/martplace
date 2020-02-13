a8e64ac8a2d835b68cd8771cdc20268e4ea3addf
OK <masha.zmi@gmail.com>
2020-02-13 18:28:49 +0200

    added advantages

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,16 @@ $(function(){
     $('.featured__slider').slick({
         
       });
+    $('.feed__inner').slick({
+        slidesToShow: 3,
+      });
 
-      $(".product__item-star").rateYo({
+    $(".product__item-star").rateYo({
         rating: 4.5,
         starWidth: "13px",
         spacing: "3px",
         readOnly: true,
         ratedFill: "#ffc000",
       });
-      var mixer = mixitup('.new__slider');
+    var mixer = mixitup('.new__slider');
 });
