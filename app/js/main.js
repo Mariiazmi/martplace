39dde61837860c09b6fc6071b2914a4cf6b444cb
OK <masha.zmi@gmail.com>
2020-02-20 17:17:34 +0200

    added single product page

--- app/js/main.js
+++ app/js/main.js
@@ -36,6 +36,14 @@ $(function(){
       $('.header__nav-list').slideToggle();
     });
 
+    $('.product__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.product__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
 
     var mixer = mixitup('.new__slider');
 });
